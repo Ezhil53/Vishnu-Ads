6372863f08a1b5de4f9f60974f888db6cd2a69fe
Ezhil <72091493+Ezhil53@users.noreply.github.com>
2025-09-24 18:53:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App () {
     <>
       <BrowserRouter>
        <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>  
        </Routes>
       </BrowserRouter>
     </>
